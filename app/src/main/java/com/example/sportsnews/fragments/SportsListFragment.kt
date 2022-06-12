dd233846c5e5f44be526a6872bd6ccc20dc28126
Imron <imron.gamidli@lt.hs-fulda.de>
2022-06-12 10:45:38 +0200

    feat: display list of news with only an image

--- app/src/main/java/com/example/sportsnews/fragments/SportsListFragment.kt
+++ app/src/main/java/com/example/sportsnews/fragments/SportsListFragment.kt
@@ -5,19 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.sportsnews.adapter.ItemAdapter
+import com.example.sportsnews.data.Datasource
+import com.example.sportsnews.databinding.FragmentSportsListBinding
+
 
 class SportsListFragment : Fragment() {
+  private lateinit var binding: FragmentSportsListBinding
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    _binding = SportsListFragment.inflate(inflater, container, false)
+    binding = FragmentSportsListBinding.inflate(inflater, container, false)
     return binding.root
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    val myDataset = Datasource().loadNews()
+    binding.recyclerView.apply {
+      adapter = ItemAdapter(requireContext(), myDataset)
+      setHasFixedSize(true)
+    }
   }
-
 }
